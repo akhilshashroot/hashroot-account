601fb7846272fcb0153a55a56ea675e32fecc6b1
lijimol-hashroot <92438222+lijimol-hashroot@users.noreply.github.com>
2022-07-29 16:22:36 +0530

    changes in invoice

--- src/pages/HashrootpInvoice/HashrootpInvoiceAdd.js
+++ src/pages/HashrootpInvoice/HashrootpInvoiceAdd.js
@@ -60,6 +60,7 @@ class HashrootInvoiceAddEdit extends React.Component {
         CGST: '',
         SGST: '',
         withoutCGST: '',
+        withoutCGSTnew: '',
         // declaration: '',
         GSTIN: '',
     };
@@ -116,7 +117,8 @@ class HashrootInvoiceAddEdit extends React.Component {
                     withoutIGST: this.props.data.amount,
                     CGST: this.props.data?.CGST,
                     SGST: this.props.data?.SGST,
-                    withoutCGST: this.props.data.amount,
+                    withoutCGST: this.props.data.amount-this.props.data.gst,
+                    //withoutCGSTnew:this.props.data.amount-this.props.data.gst,
                     hsn:this.props.data?.hsn?.hsn_id
                     // declaration: '',
                 });
