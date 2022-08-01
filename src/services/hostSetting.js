882477d40a3114831e219360f0759dace03d44db
akhilshashroot <93207850+akhilshashroot@users.noreply.github.com>
2022-08-01 10:05:02 +0530

    base url changed

--- src/services/hostSetting.js
+++ src/services/hostSetting.js
@@ -1,8 +1,8 @@
 var url = '';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    url = "http://account.localhost";
+    url = "https://accountsnew.hashroot.org/server";
 } else {
-    url = "http://account.localhost";
+    url = "https://accountsnew.hashroot.org/server";
 }
 
 export const API_BASE_URL = url;
